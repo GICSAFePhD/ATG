b798d905cbae03b234c0b7d8ca8693f0ef13c65a
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-27 15:51:55 -0300

    Main function created

---
+++ ATG.py
@@ -0,0 +1,6 @@
+def ATG():
+    print("#"*20+" Starting Automatic Test Generator "+"#"*20)
+    print("Reading railML object")
+    print("Reading test specifications")
+    print("Generating validation tests")
+    print("Generatin CoCoTB tests")
