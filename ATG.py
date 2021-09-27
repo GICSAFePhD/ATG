7540aa987301e93c04671feb1b709dfa3a182807
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-27 19:59:04 -0300

    RNA in progress

--- ATG.py
+++ ATG.py
@@ -1,6 +1,7 @@
-def ATG():
-    print("#"*20+" Starting Automatic Test Generator "+"#"*20)
-    print("Reading railML object")
-    print("Reading test specifications")
-    print("Generating validation tests")
-    print("Generatin CoCoTB tests")
+def ATG(test = False):
+    if test:
+        print("#"*20+" Starting Automatic Test Generator "+"#"*20)
+        print("Reading railML object")
+        print("Reading test specifications")
+        print("Generating validation tests")
+        print("Generatin CoCoTB tests")
